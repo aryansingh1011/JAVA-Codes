0c383d233e67f5eb1fdde8c315090899b99994d6
Aryan Singh <132202504+aryansingh1011@users.noreply.github.com>
2023-04-30 17:27:45 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+// THIS CODE IS TO PRINT A SIMPLE LINE
+// Class name is "Main" written in "Pascel Naming Conventions" eg: MyCodeSource , Codectutorial etc (Starts with UpperCase Alphabet)
+// Funtion name is "main" written in "Camel Naming Convention" eg: myCodeSource , codectutorials etc (Starts with lowerCase Alphabet)
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
+        
 
         System.out.println("Hello and welcome!");
     }
